d44d67d7e14f80050f1b24bafa74b7008b688184
aaleksopoulos <athanasios.al@gmail.com>
2020-05-11 16:15:38 +0300

    some initializations on inference.py

--- inference.py
+++ inference.py
@@ -31,23 +31,53 @@ from openvino.inference_engine import IENetwork, IECore
 class Network:
     """
     Load and configure inference plugins for the specified target devices 
-    and performs synchronous and asynchronous modes for the specified infer requests.
+    and performs asynchronous modes for the specified infer requests.
     """
 
     def __init__(self):
         ### TODO: Initialize any class variables desired ###
+        self.core = None
+        self.network = None
+        self.input_blob = None
+        self.output_blob = None
+        self.exec_network = None
 
-    def load_model(self):
+    def load_model(self, device='CPU', model_xml, cpu_extension=None):
         ### TODO: Load the model ###
         ### TODO: Check for supported layers ###
         ### TODO: Add any necessary extensions ###
         ### TODO: Return the loaded inference plugin ###
         ### Note: You may need to update the function parameters. ###
+        #check if we have provided a valid file for the xml
+        if (model_xml.endsWith('.xml') and (os.path.exists(model_xml)):
+            model_bin = model_xml.replace('.xlm', '.bin') #get the model bin
+        else:
+            print("There was a problem reading the xml file provided, exiting...")
+            exit(1)
+
+        #initialize the Inference Engine (NOTE using the 2019R3 version of OPENVino, as stated in the exercise)
+        self.core = IECore()
+
+        #add, if provided, a cpu extension
+        if (cpu_extension):
+            self.core.add_extension(cpu_extension)
+        
+        #initialize the Network (NOTE in the 2020R1 version of OPENVino, can be used from the IECore)
+        self.network = IENetwork(model=model_xml, weights=model_bin)
+
+        #load to network to get the executable network
+        self.exec_network = self.core.load_network(self.network, device)
+
+        #get the input and output blobs
+        self.input_blob = next(iter(self.network.inputs))
+        self.output_blob = next(iter(self.network.outputs))
+
         return
 
     def get_input_shape(self):
         ### TODO: Return the shape of the input layer ###
-        return
+        
+        return self.network.inputs[self.input_blob].shape
 
     def exec_net(self):
         ### TODO: Start an asynchronous request ###
